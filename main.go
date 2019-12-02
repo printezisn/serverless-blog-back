d6b047a437a95e580722546b23c06477e0440123
Nikos <printezisn@gmail.com>
2019-12-01 18:30:16 -0800

    Remove test code.

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-lambda-go/lambda"
 	regularHandler "github.com/printezisn/serverless-blog-back/blogpost/handler/regular"
 	"github.com/printezisn/serverless-blog-back/blogpost/repository/dynamodb"
@@ -13,8 +11,6 @@ func main() {
 	repo := dynamodb.New()
 	service := regularService.New(&repo)
 	handler := regularHandler.New(&service)
-
-	fmt.Println("The server starts...")
 
 	lambda.Start(handler.Handle)
 }
