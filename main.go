9ea2937414c506744a06db68df036ca26180f997
Nikos <printezisn@gmail.com>
2019-12-01 18:25:44 -0800

    Add test code to see if lambda is updated.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-lambda-go/lambda"
 	regularHandler "github.com/printezisn/serverless-blog-back/blogpost/handler/regular"
 	"github.com/printezisn/serverless-blog-back/blogpost/repository/dynamodb"
@@ -11,6 +13,8 @@ func main() {
 	repo := dynamodb.New()
 	service := regularService.New(&repo)
 	handler := regularHandler.New(&service)
+
+	fmt.Println("The server starts...")
 
 	lambda.Start(handler.Handle)
 }
